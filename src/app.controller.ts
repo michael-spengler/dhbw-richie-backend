86daa8cfef217f1d77eb6e376531d8d0b2e6a191
michael-spengler <>
2019-09-18 14:34:11 +0200

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
@@ -8,7 +8,7 @@ export class AppController {
 
   @Get('getTrainingData')
   public getTrainingData(): string {
-    const result = this.appService.getTrainingData();
-    return result;
+    const result = this.appService.getTrainingData()
+    return result
   }
 }
