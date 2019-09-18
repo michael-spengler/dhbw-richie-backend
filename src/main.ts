223eb277242ce86ab2484ab19b77e07a68cd2e9b
michael-spengler <>
2019-09-18 16:47:32 +0200

    introducing https

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as fs from 'fs'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: {origin: '*'}})
+  const httpsOptions: any = {
+    cert: fs.readFileSync(`/etc/letsencrypt/live/server-certificate/cert.pem`),
+    key: fs.readFileSync(`/etc/letsencrypt/live/server-certificate/privkey.pem`),
+  }
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  })
+  // await app.listen(3000)
   // await app.listen(80)
-  await app.listen(3000)
+  await app.listen(443)
 }
 bootstrap()
